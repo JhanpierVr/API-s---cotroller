2fee5ed787f35d6f873db4fc68bb0a45c1ac5cca
JhanpierVr <jhanpier.jack@gmail.com>
2023-10-17 01:25:36 -0500

    firsy commit

---
+++ NetCorePrueba1/Controllers/ClienteController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCorePrueba1.Models;
+
+namespace NetCorePrueba1.Controllers
+{
+    [ApiController]
+    [Route("cliente")]
+    public class ClienteController : ControllerBase
+    {
+        [HttpGet]
+        [Route("listar")]
+        public dynamic listarCliente()
+        {
+            //Agregarmos code pero siempre con return para recibir respuesta del API
+
+            List<Cliente> clientes = new List<Cliente>
+            {
+                new Cliente
+                {
+                    id = "1",
+                    nombre = "Adrian Celis",
+                    cargo = "Analista Programador",
+                    correo = "adriancelistorres184@gmail.com",
+                    edad = "20"
+                },
+                new Cliente
+                {
+                    id = "2",
+                    nombre = "Jhanpier Valdivia",
+                    cargo = "Practicante de programación",
+                    correo = "jhanpier.jack@gmail.com",
+                    edad = "21"
+                }
+            };
+
+            return clientes;
+        }
+
+        [HttpGet]
+        [Route("listarxid")]
+        public dynamic listarClientexid(int codigo)
+        {
+            //Obtenemos el cliente de la DB
+
+            return new Cliente
+            {
+                id = codigo.ToString(),
+                nombre = "Adrian Celis",
+                cargo = "Analista Programador",
+                correo = "adriancelistorres184@gmail.com",
+                edad = "20"
+            };
+        }
+
+        [HttpPost]
+        [Route("guardar")]
+        public dynamic guardarCliente(Cliente cliente)
+        {
+            //Guardamos en la BD y asignamos n° id
+            cliente.id = "3";
+
+            return new
+            {
+                success = true,
+                message = "cliente registrado",
+                result = cliente
+            };
+        }
+
+        [HttpPost]
+        [Route("eliminar")]
+        public dynamic eliminarCliente(Cliente cliente)
+        {
+            string token = Request.Headers.Where(x => x.Key == "Authorization").FirstOrDefault().Value;
+            //Eliminamos de la BD
+
+            if(token != "jhan123.")
+            {
+                return new
+                {
+                    success = false,
+                    message = "token incorrecto",
+                    result = ""
+                };
+            }
+
+            return new
+            {
+                success = true,
+                message = "cliente eliminado",
+                result = cliente
+            }; 
+        }
+    } 
+}
+
---
+++ NetCorePrueba1/Models/Cliente.cs
@@ -0,0 +1,11 @@
+﻿namespace NetCorePrueba1.Models
+{
+    public class Cliente
+    {
+        public string id { get; set; }
+        public string nombre { get; set; }
+        public string cargo { get; set; }
+        public string edad { get; set; }
+        public string correo { get; set; }
+    }
+}
